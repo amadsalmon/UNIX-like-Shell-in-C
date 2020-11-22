811e94ec316137199461a3755a254571bcf849b1
Amad Salmon <amad.salmon@icloud.com>
2020-11-23 00:42:20 +0100

    feat: add create_process function

--- src/processes.c
+++ src/processes.c
@@ -22,33 +22,40 @@ int push_pid(pid_list * head, pid_t pid) {
 
 struct process * create_process(char **words){
     struct process *p = (struct process *) malloc(sizeof(struct process));
+    p->argv = words;
     /* 1) Déterminer si words[0] builtin ou external */
-    /* 2) Parcourir tout le words en entier et chercher redirection */
+    /* 2) Parcourir words et chercher redirection */
     /* 3) Remplir p */
-    if (is_builtin(words[0]))
+
+    if (is_builtin(words))
     {
         p->type = BUILTIN_COMMAND;
         p->path = words[0];
     }
-    
     else
     {
         p->type = EXTERNAL_COMMAND;
+
+        char *path = malloc(5 + strlen(words[0]));
+        strcat(path, "/bin/");
+        strcat(path, words[0]);
+        p->path = path;
 
         int i = 0;
         while (words[i] != NULL)
         {
             if (words[i][0] == '>') {
-                if (words[i+1] != NULL && words[i+1][0] == '>'){
-                    p->output = words[i+2];
-                } else
+                p->output_flag = O_CREAT | O_WRONLY;
+                p->output = words[i+1];
+                if (words[i + 1] != NULL && words[i + 1][0] == '>')
                 {
-                    p->output = words[i+1];
-                }
-                
+                    p->output = words[i+2];
+                    p->output_flag |= O_APPEND;
+                }                
                 words[i] = NULL;
                 break;
             } else if (words[i][0] == '<'){
+                p->output_flag = O_RDONLY;
                 p->input = words[i+1];
                 words[i] = NULL;
                 break;
